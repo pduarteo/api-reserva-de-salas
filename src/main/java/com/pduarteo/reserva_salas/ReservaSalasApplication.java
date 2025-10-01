1a7929be15e608ccb878219e24c5d43282921569
pedro <pedroduarteolv@gmail.com>
2025-10-01 18:48:18 -0300

    setup project

---
+++ src/main/java/com/pduarteo/reserva_salas/ReservaSalasApplication.java
@@ -0,0 +1,13 @@
+package com.pduarteo.reserva_salas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaSalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaSalasApplication.class, args);
+	}
+
+}
